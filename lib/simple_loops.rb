d4439f58da48d1af4e5acf4c6316b2de3b1776b1
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-29 02:56:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -29,7 +29,7 @@ def return_string_array (array)
   counter = 0 
   new_array = []
   while counter < array.length do
-           new_array.push()
+      new_array.push(array[counter])
            counter += 1 
         end
 end

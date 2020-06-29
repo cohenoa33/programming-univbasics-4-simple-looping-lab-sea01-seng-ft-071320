340800143752b1320fbb33e26a1aa8ac8af5f295
Noa Rabin-Cohen <cohenoa33@gmail.com>
2020-06-29 02:46:53 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,12 @@ def loop_message_five_times(message)
     counter += 1 
   end
 end
+
+
+def loop_message_n_times (message, integer)
+  counter = 0 
+  while counter < 5 do
+    puts message
+    counter += 1 
+  end
+end 
